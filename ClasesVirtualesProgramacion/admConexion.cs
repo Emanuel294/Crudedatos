0dd83c84c305484ed4a3c4944a9a303160b37238
Emanuel294 <68527491+Emanuel294@users.noreply.github.com>
2020-10-19 13:33:51 -0600

    ponemos el acceso publico y todos los using

--- ClasesVirtualesProgramacion/admConexion.cs
+++ ClasesVirtualesProgramacion/admConexion.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
 
 namespace ClasesVirtualesProgramacion
 {
     class admConexion
     {
+        public MySqlConnection oConexion;
     }
 }

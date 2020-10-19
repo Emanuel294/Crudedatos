3bf10af1045ec915bbdc4c4c27c7368ce67014f9
Emanuel294 <68527491+Emanuel294@users.noreply.github.com>
2020-10-19 14:14:52 -0600

    funciones para abrir una conexion

--- ClasesVirtualesProgramacion/admConexion.cs
+++ ClasesVirtualesProgramacion/admConexion.cs
@@ -12,5 +12,14 @@ namespace ClasesVirtualesProgramacion
     class admConexion
     {
         public MySqlConnection oConexion;
+
+        public bool AbrirConexion()
+        {
+            bool conectado = false;
+            string servidor = "localhost", puerto = "3306";
+            string usuario = "root", contrasena = "root", BD = "clasesvirtuales";
+            string cadenaConexion = string.Format("datasource={0}; port={1}; username={2}; password={3}; database={4}", servidor,puerto,usuario,contrasena,BD);
+            return conectado;
+        }
     }
 }

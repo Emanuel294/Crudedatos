ce6affb6e35285ac323bcdb68082d34c1dd612fd
Emanuel294 <68527491+Emanuel294@users.noreply.github.com>
2020-10-23 17:55:20 -0600

    terminamos de diseñar la interfas

--- ClasesVirtualesProgramacion/Dialogs/EstudianteDialog.Designer.cs
+++ ClasesVirtualesProgramacion/Dialogs/EstudianteDialog.Designer.cs
@@ -28,19 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label identidadLabel;
+            System.Windows.Forms.Label nombresLabel;
+            System.Windows.Forms.Label apellidosLabel;
+            System.Windows.Forms.Label fechanacLabel;
+            System.Windows.Forms.Label sexoLabel;
+            System.Windows.Forms.Label direccionLabel;
+            System.Windows.Forms.Label obsLabel;
+            this.identidadTextBox = new System.Windows.Forms.TextBox();
+            this.nombresTextBox = new System.Windows.Forms.TextBox();
+            this.apellidosTextBox = new System.Windows.Forms.TextBox();
+            this.fechanacDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.sexoComboBox = new System.Windows.Forms.ComboBox();
+            this.direccionTextBox = new System.Windows.Forms.TextBox();
+            this.obsTextBox = new System.Windows.Forms.TextBox();
+            identidadLabel = new System.Windows.Forms.Label();
+            nombresLabel = new System.Windows.Forms.Label();
+            apellidosLabel = new System.Windows.Forms.Label();
+            fechanacLabel = new System.Windows.Forms.Label();
+            sexoLabel = new System.Windows.Forms.Label();
+            direccionLabel = new System.Windows.Forms.Label();
+            obsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.erp)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(449, 214);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(550, 214);
+            // 
+            // identidadLabel
+            // 
+            identidadLabel.AutoSize = true;
+            identidadLabel.Location = new System.Drawing.Point(58, 25);
+            identidadLabel.Name = "identidadLabel";
+            identidadLabel.Size = new System.Drawing.Size(54, 13);
+            identidadLabel.TabIndex = 3;
+            identidadLabel.Text = "Identidad:";
+            // 
+            // identidadTextBox
+            // 
+            this.identidadTextBox.Location = new System.Drawing.Point(119, 21);
+            this.identidadTextBox.Name = "identidadTextBox";
+            this.identidadTextBox.Size = new System.Drawing.Size(127, 20);
+            this.identidadTextBox.TabIndex = 4;
+            // 
+            // nombresLabel
+            // 
+            nombresLabel.AutoSize = true;
+            nombresLabel.Location = new System.Drawing.Point(60, 51);
+            nombresLabel.Name = "nombresLabel";
+            nombresLabel.Size = new System.Drawing.Size(52, 13);
+            nombresLabel.TabIndex = 5;
+            nombresLabel.Text = "Nombres:";
+            // 
+            // nombresTextBox
+            // 
+            this.nombresTextBox.Location = new System.Drawing.Point(119, 47);
+            this.nombresTextBox.Name = "nombresTextBox";
+            this.nombresTextBox.Size = new System.Drawing.Size(200, 20);
+            this.nombresTextBox.TabIndex = 6;
+            // 
+            // apellidosLabel
+            // 
+            apellidosLabel.AutoSize = true;
+            apellidosLabel.Location = new System.Drawing.Point(341, 51);
+            apellidosLabel.Name = "apellidosLabel";
+            apellidosLabel.Size = new System.Drawing.Size(52, 13);
+            apellidosLabel.TabIndex = 7;
+            apellidosLabel.Text = "Apellidos:";
+            // 
+            // apellidosTextBox
+            // 
+            this.apellidosTextBox.Location = new System.Drawing.Point(402, 47);
+            this.apellidosTextBox.Name = "apellidosTextBox";
+            this.apellidosTextBox.Size = new System.Drawing.Size(223, 20);
+            this.apellidosTextBox.TabIndex = 8;
+            // 
+            // fechanacLabel
+            // 
+            fechanacLabel.AutoSize = true;
+            fechanacLabel.Location = new System.Drawing.Point(252, 25);
+            fechanacLabel.Name = "fechanacLabel";
+            fechanacLabel.Size = new System.Drawing.Size(63, 13);
+            fechanacLabel.TabIndex = 9;
+            fechanacLabel.Text = "Nacimiento:";
+            // 
+            // fechanacDateTimePicker
+            // 
+            this.fechanacDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.fechanacDateTimePicker.Location = new System.Drawing.Point(320, 21);
+            this.fechanacDateTimePicker.Name = "fechanacDateTimePicker";
+            this.fechanacDateTimePicker.Size = new System.Drawing.Size(85, 20);
+            this.fechanacDateTimePicker.TabIndex = 10;
+            // 
+            // sexoLabel
+            // 
+            sexoLabel.AutoSize = true;
+            sexoLabel.Location = new System.Drawing.Point(438, 25);
+            sexoLabel.Name = "sexoLabel";
+            sexoLabel.Size = new System.Drawing.Size(34, 13);
+            sexoLabel.TabIndex = 11;
+            sexoLabel.Text = "Sexo:";
+            // 
+            // sexoComboBox
+            // 
+            this.sexoComboBox.FormattingEnabled = true;
+            this.sexoComboBox.Items.AddRange(new object[] {
+            "Femenino",
+            "Masculino"});
+            this.sexoComboBox.Location = new System.Drawing.Point(477, 21);
+            this.sexoComboBox.Name = "sexoComboBox";
+            this.sexoComboBox.Size = new System.Drawing.Size(148, 21);
+            this.sexoComboBox.TabIndex = 12;
+            // 
+            // direccionLabel
+            // 
+            direccionLabel.AutoSize = true;
+            direccionLabel.Location = new System.Drawing.Point(57, 77);
+            direccionLabel.Name = "direccionLabel";
+            direccionLabel.Size = new System.Drawing.Size(55, 13);
+            direccionLabel.TabIndex = 13;
+            direccionLabel.Text = "Direccion:";
+            // 
+            // direccionTextBox
+            // 
+            this.direccionTextBox.Location = new System.Drawing.Point(119, 74);
+            this.direccionTextBox.Multiline = true;
+            this.direccionTextBox.Name = "direccionTextBox";
+            this.direccionTextBox.Size = new System.Drawing.Size(506, 64);
+            this.direccionTextBox.TabIndex = 14;
+            // 
+            // obsLabel
+            // 
+            obsLabel.AutoSize = true;
+            obsLabel.Location = new System.Drawing.Point(34, 160);
+            obsLabel.Name = "obsLabel";
+            obsLabel.Size = new System.Drawing.Size(78, 13);
+            obsLabel.TabIndex = 15;
+            obsLabel.Text = "Observaciones";
+            // 
+            // obsTextBox
+            // 
+            this.obsTextBox.Location = new System.Drawing.Point(119, 144);
+            this.obsTextBox.Multiline = true;
+            this.obsTextBox.Name = "obsTextBox";
+            this.obsTextBox.Size = new System.Drawing.Size(506, 64);
+            this.obsTextBox.TabIndex = 16;
             // 
             // EstudianteDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(589, 226);
+            this.ClientSize = new System.Drawing.Size(681, 284);
+            this.Controls.Add(identidadLabel);
+            this.Controls.Add(this.identidadTextBox);
+            this.Controls.Add(nombresLabel);
+            this.Controls.Add(this.nombresTextBox);
+            this.Controls.Add(apellidosLabel);
+            this.Controls.Add(this.apellidosTextBox);
+            this.Controls.Add(fechanacLabel);
+            this.Controls.Add(this.fechanacDateTimePicker);
+            this.Controls.Add(sexoLabel);
+            this.Controls.Add(this.sexoComboBox);
+            this.Controls.Add(direccionLabel);
+            this.Controls.Add(this.direccionTextBox);
+            this.Controls.Add(obsLabel);
+            this.Controls.Add(this.obsTextBox);
             this.Name = "EstudianteDialog";
             this.Text = "Información de estudiante";
+            this.Controls.SetChildIndex(this.obsTextBox, 0);
+            this.Controls.SetChildIndex(obsLabel, 0);
+            this.Controls.SetChildIndex(this.direccionTextBox, 0);
+            this.Controls.SetChildIndex(direccionLabel, 0);
+            this.Controls.SetChildIndex(this.sexoComboBox, 0);
+            this.Controls.SetChildIndex(sexoLabel, 0);
+            this.Controls.SetChildIndex(this.fechanacDateTimePicker, 0);
+            this.Controls.SetChildIndex(fechanacLabel, 0);
+            this.Controls.SetChildIndex(this.apellidosTextBox, 0);
+            this.Controls.SetChildIndex(apellidosLabel, 0);
+            this.Controls.SetChildIndex(this.nombresTextBox, 0);
+            this.Controls.SetChildIndex(nombresLabel, 0);
+            this.Controls.SetChildIndex(this.identidadTextBox, 0);
+            this.Controls.SetChildIndex(identidadLabel, 0);
+            this.Controls.SetChildIndex(this.btnGuardar, 0);
+            this.Controls.SetChildIndex(this.btnCancelar, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.erp)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox identidadTextBox;
+        private System.Windows.Forms.TextBox nombresTextBox;
+        private System.Windows.Forms.TextBox apellidosTextBox;
+        private System.Windows.Forms.DateTimePicker fechanacDateTimePicker;
+        private System.Windows.Forms.ComboBox sexoComboBox;
+        private System.Windows.Forms.TextBox direccionTextBox;
+        private System.Windows.Forms.TextBox obsTextBox;
     }
 }

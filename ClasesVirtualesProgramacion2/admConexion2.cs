8a903317e69ebd0014d7eb6b0e7ea27d207f2863
Emanuel294 <68527491+Emanuel294@users.noreply.github.com>
2020-10-27 18:48:32 -0600

    creamos el segundo proyecto y programamos la conexion

---
+++ ClasesVirtualesProgramacion2/admConexion2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ClasesVirtualesProgramacion2
+{
+    class admConexion2
+    {
+        public MySqlConnection oConexion2;
+
+        public bool AbrirConexion2()
+        {
+            bool conectado = false;
+            string servidor = "localhost", puerto = "3306";
+            string usuario = "root", contrasena = "root", BD = "clasesvirtuales";
+            string cadenadeconexion = string.Format("datasource= {0}; port = {1}; username={2}; passwork={3}; database={4}", servidor, puerto, usuario, contrasena, BD);
+
+            try
+            {
+                if (oConexion2 != null && oConexion2.State == ConnectionState.Open)
+                    oConexion2.Close();
+
+
+                oConexion2 = new MySqlConnection(cadenadeconexion);
+                oConexion2.Open();
+                conectado = true;
+             
+            }
+            catch (MySqlException Exception)
+            {
+
+                MessageBox.Show(Exception.Message, "Error en conexión", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conectado = false;
+            }
+            return conectado;
+
+        }
+    }
+}
